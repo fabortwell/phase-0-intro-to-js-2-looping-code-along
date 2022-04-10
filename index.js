07349dd743fa3fbe3d003d69b8f26ee37fa0a57f
Brian Sitati <briansit93@gmail.com>
2022-04-10 12:29:11 +0300

    completed assignment

--- index.js
+++ index.js
@@ -3,4 +3,20 @@ for (let age = 30; age < 40; age++) {
     console.log(`I'm ${age} years old. Happy birthday to me!`);
     debugger;
   }
+
+  function writeCards( namesArray, event ) {
+    let thankYouCards = []
+    for ( let i = 0; i < namesArray.length; i++ ) {
+      thankYouCards.push( `Thank you, ${namesArray[i]}, for the wonderful ${event} gift!` )
+    }
+    return thankYouCards
+  }
+  
+  function countDown( startingNumber ) {
+    while ( startingNumber > 0 ) {
+      console.log( startingNumber );
+      startingNumber -= 1;
+    }
+    console.log( startingNumber );
+  }
   
